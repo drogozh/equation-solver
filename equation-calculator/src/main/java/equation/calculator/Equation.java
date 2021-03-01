c75cfaff0663b41005b216c798a12ebdc7740ba6
Dmitry Rogozhkin <dmitry@unioninformatics.com>
2021-03-01 11:03:18 -0500

    Added explainer javadoc

--- equation-calculator/src/main/java/equation/calculator/Equation.java
+++ equation-calculator/src/main/java/equation/calculator/Equation.java
@@ -1,9 +1,27 @@
 package equation.calculator;
 
+import equation.exceptions.EquationDefinitionException;
+
 public abstract class Equation {
     protected Coefficient[] coefficients;
     public abstract Coefficient[] solve();
-    public Equation(Coefficient[] terms){
-        coefficients = terms;
+
+    /**
+     * Constructs instances of Equation interface
+     * @param coefficients Coefficient's power matches its position in the array;
+     *                     for example: ax^2 + bx^1 + cx^0 = 0 coefficient packing is [c,b,a]
+     * @return Equation
+     * @throws EquationDefinitionException
+     */
+    public Equation(Coefficient[] coefficients){
+        this.coefficients = coefficients;
+    }
+
+    public Coefficient[] getCoefficients() {
+        Coefficient[] copy = new Coefficient[coefficients.length];
+        for(int index=0; index < coefficients.length; index++) {
+            copy[index] = coefficients[index];
+        }
+        return copy;
     }
 }
--- equation-calculator/src/main/java/equation/calculator/EquationFactory.java
+++ equation-calculator/src/main/java/equation/calculator/EquationFactory.java
@@ -6,6 +6,13 @@ public class EquationFactory {
     private final int LINEAR_EQUATION = 2;
     private final int QUADRATIC_EQUATION = 3;
 
+    /**
+     * Constructs instances of Equation interface
+     * @param coefficients Higher order coefficients occupy lower position index, i.e. left to right;
+     *                     for example: ax^2 + bx + c = 0 coefficient packing is [a,b,c]
+     * @return Equation
+     * @throws EquationDefinitionException
+     */
     public Equation makeEquation(Coefficient[] coefficients) throws EquationDefinitionException {
         switch (coefficients.length) {
             case LINEAR_EQUATION:
